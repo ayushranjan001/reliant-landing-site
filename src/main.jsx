c91867f49e0569c201ee1402f51d3556b1029bff
Ayush Ranjan <117291986+ayushranjan001@users.noreply.github.com>
2026-09-21 19:48:56 +0530

    feat: build premium tuition site shell

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { store } from './store';
-import { theme } from './theme';
-import './styles.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
