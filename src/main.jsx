06cfe31951ab136d325bae9a234e112f35cdc6d6
Ayush Ranjan <117291986+ayushranjan001@users.noreply.github.com>
2026-09-21 19:10:21 +0530

    Rebuild site as modern React home tuition landing page

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Provider } from 'react-redux';
+import App from './App';
+import { store } from './store';
+import { theme } from './theme';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>,
+);
